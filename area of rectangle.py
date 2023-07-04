8f41f06df23babccfa3ccf13112b618887d830b2
avishnasree <avishnasree8@gmail.com>
2023-07-04 10:19:11 -0800

    first commit

---
+++ area of rectangle.py
@@ -0,0 +1,5 @@
+length = float(input("enter the length of the rectangle:"))
+width = float(input("enter the width of the rectangle:"))
+area = length * width
+
+print("the area of the rectangle is:", area)
---
+++ area of square.py
@@ -0,0 +1,3 @@
+side = float(input("enter the length of a side of a square:"))
+area = side * side
+print("the area of the sqare is:",area)
